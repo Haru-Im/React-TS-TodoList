10fc2be5cd01931b8b74185aef2b927da9026747
Haru Im <chalk323@naver.com>
2023-06-25 23:14:54 +0900

    switch from client-side state updates to axios API calls

--- src/pages/todo-list/container/hooks/todos.hook.ts
+++ src/pages/todo-list/container/hooks/todos.hook.ts
@@ -4,29 +4,14 @@ import axios from "axios";
 
 export const useTodos = () => {
   // 프론트에서 상태관리
-  const [todos, setTodos] = useState<TodoType[]>([
-    {
-      id: Number(new Date()) + 1,
-      title: "[SAMPLE] 완료한 Todo",
-      desc: "이 Todo를 지워보세요!",
-      isDone: true,
-    },
-    {
-      id: Number(new Date()),
-      title: "[SAMPLE] 완료하지 못한 Todo",
-      desc: "이 Todo를 지워보세요!",
-      isDone: false,
-    },
-  ]);
+  const [todos, setTodos] = useState<TodoType[]>([]);
   const doneTodos = todos.filter((e) => e.isDone);
   const notDoneTodos = todos.filter((e) => !e.isDone);
 
   const addTodo = (title: string, desc: string) => {
-    // json-server사용시
-
     axios
-      .post(`${process.env.REACT_APP_API}/todos`, {
-        id: Number(new Date()),
+      .post(`${process.env.REACT_APP_TODOS}/todos`, {
+        // id: Number(new Date()),
         title: title,
         desc: desc,
         isDone: false,
@@ -37,23 +22,21 @@ export const useTodos = () => {
       })
       .catch((error) => console.error("Error:", error));
 
-    setTodos([
-      ...todos,
-      {
-        id: Number(new Date()),
-        title: title,
-        desc: desc,
-        isDone: false,
-      },
-    ]);
+    // setTodos([
+    //   ...todos,
+    //   {
+    //     id: Number(new Date()),
+    //     title: title,
+    //     desc: desc,
+    //     isDone: false,
+    //   },
+    // ]);
   };
 
   const deleteTodo = async (id: number) => {
     try {
-      // json-server 사용시
-
       const response = await axios.delete(
-        `${process.env.REACT_APP_API}/todos/${id}`
+        `${process.env.REACT_APP_TODOS}/todos/${id}`
       );
       console.log(response.data);
       setTodos(todos.filter((e) => e.id !== id));
@@ -66,11 +49,11 @@ export const useTodos = () => {
     const changedStatus = { ...e, isDone: !e.isDone };
 
     try {
-      // json-server 사용시
-
       const response = await axios.patch(
-        `${process.env.REACT_APP_API}/todos/${e.id}`,
-        { isDone: changedStatus.isDone }
+        `${process.env.REACT_APP_TODOS}/todos/${e.id}`,
+        {
+          isDone: changedStatus.isDone,
+        }
       );
       if (response.status === 200) {
         setTodos(
@@ -79,8 +62,6 @@ export const useTodos = () => {
       } else {
         throw new Error("Failed to update the status on the server.");
       }
-
-      setTodos(todos.map((todo) => (todo.id === e.id ? changedStatus : todo)));
     } catch (error) {
       console.error(error);
     }
@@ -89,8 +70,9 @@ export const useTodos = () => {
   // json-server 사용시
 
   useEffect(() => {
+    console.log(process.env.REACT_APP_TODOS);
     const getTodos = async () => {
-      const response = await axios.get(`${process.env.REACT_APP_API}/todos`);
+      const response = await axios.get(`${process.env.REACT_APP_TODOS}/todos`);
       setTodos(response.data);
     };
 
