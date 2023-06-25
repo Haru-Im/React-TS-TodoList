46373ae36b03505264f9ea9aae62dc4d14cfda54
Haru Im <chalk323@naver.com>
2023-06-25 22:02:50 +0900

    refactor: url, gitignore modify for json server deploy

--- src/pages/todo-list/container/hooks/todos.hook.ts
+++ src/pages/todo-list/container/hooks/todos.hook.ts
@@ -24,18 +24,18 @@ export const useTodos = () => {
   const addTodo = (title: string, desc: string) => {
     // json-server사용시
 
-    // axios
-    //   .post("http://localhost:3001/todos", {
-    //     id: Number(new Date()),
-    //     title: title,
-    //     desc: desc,
-    //     isDone: false,
-    //   })
-    //   .then((res) => {
-    //     console.log(res.data);
-    //     setTodos([...todos, res.data]);
-    //   })
-    //   .catch((error) => console.error("Error:", error));
+    axios
+      .post(`${process.env.REACT_APP_API}/todos`, {
+        id: Number(new Date()),
+        title: title,
+        desc: desc,
+        isDone: false,
+      })
+      .then((res) => {
+        console.log(res.data);
+        setTodos([...todos, res.data]);
+      })
+      .catch((error) => console.error("Error:", error));
 
     setTodos([
       ...todos,
@@ -52,8 +52,10 @@ export const useTodos = () => {
     try {
       // json-server 사용시
 
-      // const response = await axios.delete(`http://localhost:3001/todos/${id}`);
-      // console.log(response.data);
+      const response = await axios.delete(
+        `${process.env.REACT_APP_API}/todos/${id}`
+      );
+      console.log(response.data);
       setTodos(todos.filter((e) => e.id !== id));
     } catch (error) {
       console.error(error);
@@ -66,17 +68,17 @@ export const useTodos = () => {
     try {
       // json-server 사용시
 
-      // const response = await axios.patch(
-      //   `http://localhost:3001/todos/${e.id}`,
-      //   { isDone: changedStatus.isDone }
-      // );
-      // if (response.status === 200) {
-      //   setTodos(
-      //     todos.map((todo) => (todo.id === e.id ? changedStatus : todo))
-      //   );
-      // } else {
-      //   throw new Error("Failed to update the status on the server.");
-      // }
+      const response = await axios.patch(
+        `${process.env.REACT_APP_API}/todos/${e.id}`,
+        { isDone: changedStatus.isDone }
+      );
+      if (response.status === 200) {
+        setTodos(
+          todos.map((todo) => (todo.id === e.id ? changedStatus : todo))
+        );
+      } else {
+        throw new Error("Failed to update the status on the server.");
+      }
 
       setTodos(todos.map((todo) => (todo.id === e.id ? changedStatus : todo)));
     } catch (error) {
@@ -86,14 +88,14 @@ export const useTodos = () => {
 
   // json-server 사용시
 
-  // useEffect(() => {
-  //   const getTodos = async () => {
-  //     const response = await axios.get("http://localhost:3001/todos");
-  //     setTodos(response.data);
-  //   };
+  useEffect(() => {
+    const getTodos = async () => {
+      const response = await axios.get(`${process.env.REACT_APP_API}/todos`);
+      setTodos(response.data);
+    };
 
-  //   getTodos();
-  // }, []);
+    getTodos();
+  }, []);
 
   return {
     todos,
